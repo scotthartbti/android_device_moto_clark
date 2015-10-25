1a2f588b4223a24acc664609a0ffd839f0a09a34
scott <scott@ubuntu>
2015-10-25 14:53:20 -0400

    Fix derps
    Signed-off-by: scott <scott@ubuntu>

--- device.mk
+++ device.mk
@@ -290,6 +290,5 @@ PRODUCT_COPY_FILES += \
 # setup dalvik vm configs.
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
-$(call inherit-product-if-exists, hardware/qcom/msm8x94/msm8x84.mk)
-$(call inherit-product-if-exists, vendor/qcom/gpu/msm8x94/msm8x84-gpu-vendor.mk)
-
+$(call inherit-product-if-exists, hardware/qcom/msm8x84/msm8x84.mk)
+$(call inherit-product-if-exists, vendor/qcom/gpu/msm8x84/msm8x84-gpu-vendor.mk)
