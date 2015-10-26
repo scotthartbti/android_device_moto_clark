d191dcc9f7d71872bf74dd3261c9e0ebb498df0a
scott <scott@ubuntu>
2015-10-26 02:51:28 -0400

    Revert "updated GPS"
    This reverts commit 4515987162b777c8e1da273348873d33553d14b5.

--- gps/Android.mk
+++ gps/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
-LOC_PATH := $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
-include $(call first-makefiles-under,$(LOC_PATH))
+ifeq ($(TARGET_DEVICE),clark)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
